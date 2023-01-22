63409ad113c01da0f00d2b2a6eeba17b8c978b93
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-23 00:32:29 +0300

    Выполнил третье задание

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Write("Введите день недели: ");
+int day = int.Parse(ReadLine());
+if ((day < 1) || (day > 7))
+{
+    WriteLine("Введен неверный день недели!");
+}
+else
+{
+    if ((days[day - 1] == "Суббота") || (days[day - 1] == "Воскресенье"))
+    {
+        WriteLine("Уррра!! Выходной");
+    }
+    else
+    {
+        WriteLine("Вставай на работу!");
+    }
+}
+
